c8fb60faea45a6d70f4547f3867645d48f278b16
Eduardo Cabral <eduardocabra.1105@gmail.com>
2025-06-01 14:58:13 -0300

    continuação da função de criar novos itens criando o objeto de cada item para ser adicionado noarray de itrm

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ const items = []
 function addItme(){
     const itemName = document.querySelector("#item").value
 
+    const item = {
+        name: itemName,
+        checked:false
+    }
+
+    items.push(item)
+
+    
 }
